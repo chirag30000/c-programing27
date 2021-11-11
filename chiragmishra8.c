162d74ff1d479705070424403e32cbfeb19a8bde
chirag mishra <92501622+chirag30000@users.noreply.github.com>
2021-11-11 11:22:18 +0530

    Add files via upload

---
+++ chiragmishra8.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+char character;
+printf("enter a character");
+scanf("%c",&character);
+switch(character)
+{
+case'a':printf("char is vovel");
+            break;
+case'e':printf("char is vovel");
+            break;
+case'i':printf("char is vovel");
+            break;
+case'o':printf("char is vovel");
+            break;
+case'u':printf("char is vovel");
+            break;
+case'A':printf("char is vovel");
+            break;
+case'E':printf("char is vovel");
+            break;
+case'I':printf("char is vovel");
+            break;
+case'O':printf("char is vovel");
+            break;
+case'U':printf("char is vovel");
+             break;
+default:printf("char is consunent");
+}
+return 0;
+}            
